d4878b83a9891c435fc29d57aac6c06ff3e773e7
Kaique da Silva <kaique.t2612.beyond@xpi.com.br>
2021-08-25 14:08:49 -0300

    submit: included the content of submit
    Implemented a basic form of content

--- app.ts
+++ app.ts
@@ -1,48 +1,41 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
 import staticFiles from "https://deno.land/x/static_files@1.1.0/mod.ts";
-import { parseFeed } from "https://deno.land/x/rss/mod.ts";
 
 const handle = new Handlebars();
 const router = new Router();
 
-function cleanUrl(url: URL): string {
-  let address = '';
-  const pattern = 'www.'
-  if (url.host.indexOf(pattern) > -1) {
-    address = url.host.slice(pattern.length);
-  } else {
-    address = url.host;
-  }
-  return address;
-}
+// function cleanUrl(url: URL): string {
+//   let address = '';
+//   const pattern = 'www.'
+//   if (url.host.indexOf(pattern) > -1) {
+//     address = url.host.slice(pattern.length);
+//   } else {
+//     address = url.host;
+//   }
+//   return address;
+// }
+
+const title = 'HN Clone'
+
+const news = [{
+  rank: 1,
+  url: 'https://google.com',
+  tagline: 'Google',
+  address: 'google.com',
+  points: 0
+}];
 
 router
   .get('/', async (context: any) => {
-    const response = await fetch('https://news.ycombinator.com/rss');
-    const xml = await response.text();
-    const feed = await parseFeed(xml);
-    const news: any = [];
-
-    feed.entries.forEach((entry, index) => {
-      const href = entry.links[0]['href'];
-      let address = null;
-      if (href) {
-        const url = new URL(href);
-        address = cleanUrl(url);
-      }
-      news.push({
-        rank: index + 1,
-        url: href,
-        tagline: (entry.title && entry.title.value) || href,
-        address,
-        points: 0
-      });
-    });
-
     context.response.body = await handle.renderView('index', {
-      title: 'HN Clone',
+      title,
       news: news
+    });
+  })
+  .get('/submit', async (context: any) => {
+    context.response.body = await handle.renderView('submit', {
+      title
     });
   });
 
