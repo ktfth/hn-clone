08cef86435104a3d3e66967b86dfa32aa1b6a3b2
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-08-30 12:56:04 -0300

    app: fine tune on auth
    making a fine tune on auth for all operations involved

--- app.ts
+++ app.ts
@@ -2,7 +2,8 @@ import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
 import { moment } from "https://deno.land/x/deno_moment/mod.ts";
 import { MongoClient } from "https://deno.land/x/mongo@v0.24.0/mod.ts";
-import { create, verify } from "https://deno.land/x/djwt/mod.ts"
+import { create, verify } from "https://deno.land/x/djwt/mod.ts";
+import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
 
 const key = await crypto.subtle.generateKey(
   { name: "HMAC", hash: "SHA-512" },
@@ -26,7 +27,13 @@ interface News {
   displayDate: string | null;
 }
 
+interface User {
+  acct: string;
+  pw: string;
+}
+
 const news = db.collection<News>('news');
+const users = db.collection<User>('users');
 
 const handle = new Handlebars();
 const router = new Router();
@@ -64,20 +71,59 @@ router
     });
   })
   .get('/auth', async (context) => {
-    context.response.body = await handle.renderView('auth');
+    context.response.body = await handle.renderView('auth', {
+      message: await context.cookies.get('message')
+    });
   })
   .post('/auth', async (context) => {
     const result = context.request.body();
     const value = await result.value;
-    console.log(value.get('creating'));
-    const jwt = await create({
-      alg: 'HS512',
-      typ: 'JWT'
-    }, {
-      acct: value.get('acct'),
-      pw: value.get('pw'),
-    }, key);
-    context.response.redirect('/');
+    if (context.state.auth) {
+      context.cookies.set('message', 'User authenticated');
+      context.response.redirect('/');
+    } else {
+      if (value.get('creating') === 't') {
+        const hasUser = await users.findOne({ acct: value.get('acct') });
+        if (hasUser) {
+          context.cookies.set('message', 'User exists');
+          context.response.redirect('/auth')
+        } else {
+          const user = await users.insertOne({
+            acct: value.get('acct'),
+            pw: await bcrypt.hash(value.get('pw')),
+          });
+          context.cookies.set('message', `User created: ${value.get('acct')}`);
+          context.response.redirect('/auth');
+        }
+      } else {
+        const user = await users.findOne({ acct: value.get('acct') });
+        if (user && await bcrypt.compare(value.get('pw'), user.pw)) {
+          const jwt = await create({
+            alg: 'HS512',
+            typ: 'JWT'
+          }, {
+            acct: value.get('acct'),
+            exp: new Date().getTime() * 60000,
+            iss: 'ted'
+          }, key);
+          context.cookies.set('token', jwt);
+          context.response.redirect('/submit');
+        } else {
+          context.cookies.set('message', 'Wrong password');
+          context.response.redirect('/auth');
+        }
+      }
+    }
+  })
+  .get('/logout', async (context) => {
+    if (context.state.auth) {
+      context.cookies.set('token', '');
+      context.cookies.set('message', 'Logged out');
+      context.response.redirect('/');
+    } else {
+      context.cookies.set('message', 'Please log in');
+      context.response.redirect('/');
+    }
   })
   .post('/r', async ({request, response, cookies}) => {
     const result = request.body();
@@ -99,10 +145,15 @@ const app = new Application();
 app.use(async (context, next) => {
   const token = await context.cookies.get('token') || '';
   if (token) {
-    const result = await verify(token, key);
-    if (result) {
-      context.state.auth = true;
-      context.state.username = result.username;
+    try {
+      const result = await verify(token, key);
+      if (result) {
+        context.state.auth = true;
+        context.state.username = result.username;
+      }
+    } catch (err) {
+      context.cookies.set('token', '');
+      context.cookies.set('message', '');
     }
   }
   await next();
